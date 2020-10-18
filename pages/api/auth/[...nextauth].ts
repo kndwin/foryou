54de12e20de68f460b7621ba8d038b534b3929c8
Kevin Nguyen <me@kndwin.dev>
2020-10-18 22:49:33 +1100

    🔨 fixed type error for process.env variables in [...nextauth].js

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,17 +5,17 @@ import { NextApiRequest, NextApiResponse } from 'next'
 const options = {
   providers: [
     Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!
     }),
-    //Providers.Google({
-      //clientId: process.env.GOOGLE_ID,
-      //clientSecret: process.env.GOOGLE_SECRET
-    //}),
-    //Providers.Twitter({
-      //clientId: process.env.TWITTER_ID,
-      //clientSecret: process.env.TWITTER_SECRET
-    //}),
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!
+    }),
+    Providers.Twitter({
+      clientId: process.env.TWITTER_ID!,
+      clientSecret: process.env.TWITTER_SECRET!
+    }),
   ],
   secret: process.env.SECRET,
   session: { jwt: true }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout'
 
 export default function IndexPage () {
+  console.log(process.env.GITHUB_ID)
   return (
     <Layout title="For you">
       <h1>For you 👈</h1>
